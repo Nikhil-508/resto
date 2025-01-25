995a6f17525b06fd44d9e5d72aa3193f720b4c59
Nikhil <nikhilbchandran@gmail.com>
2025-01-25 14:43:27 +0530

    first commit

---
+++ client/src/Assets.jsx
@@ -0,0 +1,18 @@
+import logo from './assets/Logo (1).png'
+import banner from './assets/banner.png'
+import buttonbg from './assets/button-bg.png'
+import mainBg from './assets/mainbg.png'
+import line from './assets/line.png'
+import img1 from './assets/img1.png'
+import img2 from './assets/img2.png'
+
+
+export const Assets = {
+  logo,
+  banner,
+  buttonbg,
+  mainBg,
+  line,
+  img1,
+  img2
+}
